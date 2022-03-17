7035096aa623a12f64b8d8dc65f25adbbbe3b551
femizi <obito.femi@gmail.com>
2022-03-17 16:43:03 +0100

    randomness introduced

--- main.go
+++ main.go
@@ -3,38 +3,57 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/websocket"
-	
 )
+
+
 var upgrader = websocket.Upgrader{
-    ReadBufferSize:  1024,
-    WriteBufferSize: 1024,
-	CheckOrigin: func(r *http.Request) bool {return true},
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
+
+func createRandomness() string{
+	rand.Seed(time.Now().UnixNano())
+	 randomnumber := rand.Float64()
+	
+	 
+	return fmt.Sprintf("%f", randomnumber)
+
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	
+
 }
-func reader (conn * websocket.Conn){
-	for{
+func reader(conn *websocket.Conn) {
+	
+	for {
 		messageType, p, err := conn.ReadMessage()
 		if err != nil {
 			log.Println(err)
 			return
 		}
+		if string(p)== "reset"{
+			conn.WriteMessage(messageType, []byte(createRandomness()))
+
+		}
 		log.Println(string(p))
 
-		if err := conn.WriteMessage(messageType, p); err!= nil{
+		if err := conn.WriteMessage(messageType, p); err != nil {
 			log.Println(err)
 
 		}
+		conn.WriteMessage(messageType, []byte(createRandomness()))
+
 	}
 }
 
 func wsEndpoint(w http.ResponseWriter, r *http.Request) {
-    ws, err := upgrader.Upgrade(w, r, nil)
+	ws, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println(err)
 		return
@@ -44,12 +63,13 @@ func wsEndpoint(w http.ResponseWriter, r *http.Request) {
 }
 
 func setupRoutes() {
-    http.HandleFunc("/", homePage)
-    http.HandleFunc("/ws", wsEndpoint)
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/ws", wsEndpoint)
 }
 
 func main() {
-    fmt.Println("Go websockets")
+	fmt.Println("Go websockets")
+	fmt.Println(createRandomness())
 	setupRoutes()
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
